32c2b61d6ad03ea8302386389a82e3ac6ef28274
Sakshi <sakshigorkhe67@gmail.com>
2026-05-12 23:00:56 +0530

    initial commit

---
+++ UserService/UserService/src/main/java/com/lcwd/user/service/entities/User.java
@@ -0,0 +1,22 @@
+package com.lcwd.user.service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Setter;
+
+@Entity
+@Table(name="micro_users")
+@Setter
+public class User {
+    @Id
+    @Column(name="ID")
+    private String userId;
+    @Column(name="NAME",length = 20)
+    private String name;
+    @Column(name="EMAIL")
+    private String email;
+    @Column(name="ABOUT")
+    private String about;
+}
---
+++ UserService/UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.lcwd.user.service.services.impl;
+
+import com.lcwd.user.service.entities.User;
+import com.lcwd.user.service.repositories.UserRepository;
+import com.lcwd.user.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return List.of();
+    }
+
+    @Override
+    public User getUser(String userId) {
+        return null;
+    }
+
+
+}
