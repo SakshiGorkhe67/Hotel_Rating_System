47e422c9dc035a8ebf326c36333314f40fc71056
Sakshi <sakshigorkhe67@gmail.com>
2026-05-13 00:50:19 +0530

    user controller

--- UserService/UserService/src/main/java/com/lcwd/user/service/entities/User.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/entities/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name="micro_users")
 @Setter
+@Getter
 public class User {
     @Id
     @Column(name="ID")
--- UserService/UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,30 +1,37 @@
 package com.lcwd.user.service.services.impl;
 
 import com.lcwd.user.service.entities.User;
+import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+
+
     @Override
     public User saveUser(User user) {
-        return null;
+        String randomuid=UUID.randomUUID().toString();
+        user.setUserId(randomuid);
+
+        return userRepository.save(user);
     }
 
     @Override
     public List<User> getAllUser() {
-        return List.of();
+        return userRepository.findAll();
     }
 
     @Override
     public User getUser(String userId) {
-        return null;
+        return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id not found on server!!"+userId));
     }
 
 
