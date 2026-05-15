f3a3d6a714472e060a44b710768919635c72831e
Sakshi <sakshigorkhe67@gmail.com>
2026-05-16 01:49:47 +0530

    complete rating service

---
+++ HotelService/HotelService/src/test/java/com/lcwd/hotelservice/HotelService/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.hotelservice.HotelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
